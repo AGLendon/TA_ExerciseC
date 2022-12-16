9b44ce7f2e70da52e988f4080c00d0d541f95d0b
SBIID <118436683+SBIID@users.noreply.github.com>
2022-12-16 15:44:58 +0100

    hiii

---
+++ C2.m
@@ -0,0 +1,15 @@
+%% Q2
+clear; clc; close all;
+%%
+h = 0.05;   %m          -depth of beam
+rho = 7850; %kg/m^3     -plate density
+E = 2e11;   %N/m^2      -Youngs modulus
+mu = 0.3;   %           -Poisson Ratio
+c_0 = 344;  %m/s        -speed of sound in air
+
+m = rho*h;
+B = (E*h^3)/(12*(1-mu^2));
+
+omega_c = sqrt(c_0^4*m/B);  %rad/s   critical frequency
+
+f_c=omega_c/(2*pi);
