698154bac8187e0995dd058255e042275a254878
AGLendon <lendon.alex@gmail.com>
2022-12-16 17:08:53 +0100

    gjgj

--- C2.m
+++ C2.m
@@ -13,3 +13,17 @@ B = (E*h^3)/(12*(1-mu^2));
 omega_c = sqrt(c_0^4*m/B);  %rad/s   critical frequency
 
 f_c=omega_c/(2*pi);
+%% b
+f = 500;        %Hz         -Driving frequency
+omega = 2*pi*f; %rad/s      -Driving frequency
+
+k_0 = omega/c_0;
+k_B = (m/B*omega^2)^0.25;
+
+%K_0 > k_b eq 10.42
+theta = asind(k_B/k_0);
+figure;
+rangek0= 0:0.001:k_0;
+polarplot(theta+zeros(size(rangek0)),rangek0)
+%%
+
